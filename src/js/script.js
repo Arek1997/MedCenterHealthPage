96fa5d6888d38a176c122f7e0b8f8c36ad451bce
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2022-08-20 16:03:18 +0100

    Implement animated nav, depends on scroll position

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const body = document.body;
 const navLogo = document.querySelector('.nav__logo-img');
 const offersOne = document.querySelector('#offers-one');
 const headerBtn = document.querySelector('.button');
@@ -7,6 +8,9 @@ const burgerBtn = document.querySelector('.nav__burger');
 const mobileMenu = document.querySelector('.nav__menu--mobile');
 const mobileLinks = mobileMenu.querySelectorAll('a');
 const footerYear = document.querySelector('.footer__year');
+let initialScrollPos = window.scrollY;
+
+footerYear.textContent = new Date().getFullYear();
 
 const handleNav = function () {
 	document.body.classList.toggle('turnOffScroll');
@@ -28,11 +32,22 @@ const addAnimation = function () {
 	});
 };
 
+const hideNav = function () {
+	const currentScrollPos = window.scrollY;
+
+	initialScrollPos < currentScrollPos
+		? body.classList.add('hideNav')
+		: body.classList.remove('hideNav');
+
+	initialScrollPos = currentScrollPos <= 0 ? 0 : currentScrollPos;
+};
+
+window.addEventListener('scroll', hideNav);
+
 mobileMenu.addEventListener('click', function (e) {
 	e.target.matches('a') && handleNav();
 });
 
-footerYear.textContent = new Date().getFullYear();
 headerBtn.addEventListener('click', () => {
 	offersOne.scrollIntoView({ behavior: 'smooth' });
 });
